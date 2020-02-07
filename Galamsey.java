7a6bc68b3e3e7fbdb77e417139a96ef2815799e3
knoAshesi22 <46487648+knoAshesi22@users.noreply.github.com>
2020-02-07 20:09:48 +0000

    Add files via upload

---
+++ Galamsey.java
@@ -0,0 +1,118 @@
+
+//Fields: vegetation colour, colour value, position (latitude and longitude) and year of the event.
+//[6 points] Methods: Accessor (2), Mutator (2), Inherited/Auxiliary (1), Constructor (1)
+
+import java.time.Year;
+import java.util.Calendar;
+import java.util.Objects;
+
+public class Galamsey {
+
+    private enum COLOURS{GREEN,YELLOW,BROWN,NA};
+    private COLOURS vegColour;
+    private int colourValue;
+    private Position position;
+    private Calendar yearv2;
+    private Year year;
+
+
+    public Galamsey(String vegColour, Position position, Year year) {
+        this.vegColour = genColourEnum(vegColour);
+        this.position = position;
+        this.year = year;
+        this.colourValue=genColourValue(this.vegColour);
+    }
+
+
+    public COLOURS getVegColour() {
+        return vegColour;
+    }
+
+    public COLOURS genColourEnum(String vegColour){
+        try{
+            this.vegColour=COLOURS.valueOf(vegColour);
+        }
+        catch(Exception e) {
+            this.vegColour=COLOURS.NA;
+        }
+
+        return this.vegColour;
+    }
+
+    public int genColourValue(COLOURS vegColour){
+        switch (vegColour){
+            case GREEN:
+                colourValue=1;
+            case YELLOW:
+                colourValue=2;
+            case BROWN:
+                colourValue=3;
+            default:
+                colourValue=0;
+
+        }
+
+        return colourValue;
+    }
+
+    public void setVegColour(COLOURS vegColour) {
+        this.vegColour = vegColour;
+    }
+
+    public int getColourValue() {
+        return colourValue;
+    }
+
+    public void setColourValue(int colourValue) {
+        this.colourValue = colourValue;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        String ans;
+        ans="Galamsey{" +
+                "vegColour=" + vegColour +
+                ", colourValue=" + colourValue +
+                ", position=" + position +
+                ", year=" + year +
+                '}';
+        return ans;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Galamsey)) return false;
+        Galamsey galamsey = (Galamsey) o;
+        return colourValue == galamsey.colourValue &&
+                vegColour == galamsey.vegColour &&
+                Objects.equals(position, galamsey.position) &&
+                Objects.equals(year, galamsey.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(vegColour, colourValue, position, year);
+    }
+
+
+
+
+
+}
---
+++ Monitoring.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Monitoring {
+
+    private Observatory[] observatories;
+    static final String fileName="observatories.txt";
+
+
+    private static void loadObservatories(){
+
+//        try {
+//            File file=new File(fileName);
+//            Scanner orders=new Scanner(file);
+//            while (orders.hasNext()){
+//                String[] info=orders.nextLine().split("\t");
+//                try {
+//                    Order order=new Order(info[0],Double.parseDouble(info[1]),Integer.parseInt(info[2]));
+//                    System.out.println(order);
+//                }
+//                catch (Exception e){
+//                    System.out.println("Invalid row");
+//                }
+//
+//
+//            }
+//
+//        } catch (Exception e) {
+//            File file=new File(fileName);
+//            e.printStackTrace();
+//            readOrders();
+//        }
+    }
+}
