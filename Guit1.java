c7a2f4442910dd62bf66a861b678376f802d1a80
paaks100 <57193661+paaks100@users.noreply.github.com>
2020-02-20 13:30:11 +0000

    Update Guit1.java

--- Guit1.java
+++ Guit1.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -18,174 +19,146 @@ import java.time.Year;
 public class Guit1 {
     static Stage nWin;
     static Scene initScene;
-    static GridPane maingridPane;
+    static BorderPane mainPane;
+    static Monitoringv2 monitor;
 
     public static void createNewObsrv() {
+        monitor=new Monitoringv2();
         nWin = new Stage();
         nWin.initModality(Modality.APPLICATION_MODAL);
-        nWin.setMinWidth(300);
-        //creating label for text fields
-        Text text1 = new Text("Name");
-        Text text2 = new Text("Country");
-        Text text3 = new Text("Start year");
-        Text text4 = new Text("Area");
+        nWin.setMinWidth(1000);
 
-        //Creating Text Filed for observatory data
+        mainPane = new BorderPane();
+        GridPane form = new GridPane();
+        VBox rightpane = new VBox(5);
+        mainPane.setLeft(form);
+        mainPane.setCenter(rightpane);
+
+        Label l4 = new Label("Enter Observatory details");
+        Text t8 = new Text("Name");
+        Text t9 = new Text("Country");
+        Text t10 = new Text("Start year");
+        Text t11 = new Text("Area");
         TextField textField1 = new TextField();
         TextField textField2 = new TextField();
         TextField textField3 = new TextField();
         TextField textField4 = new TextField();
 
-        //Creating Buttons
-        Button button1 = new Button("Save");
-        Button button2 = new Button("Next");
+        Label l1 = new Label("Enter event Details");
+        Text t4 = new Text("Latitude");
+        Text t5 = new Text("Longitude");
+        Text t6 = new Text("Start year");
+        Text t7 = new Text("Vegetation colour");
+        TextField textField5 = new TextField();
+        TextField textField6 = new TextField();
+        TextField textField7 = new TextField();
+        TextField textField8 = new TextField();
 
-        //Creating a Grid Pane
-        maingridPane = new GridPane();
+        Label l3 = new Label("Observatory name:");
+        Label l2 = new Label("Events");
+        Text t1 = new Text("Start year");
+        Text t2 = new Text("Coordinates");
+        Text t3 = new Text("Vegetation colour");
 
-        //Setting size for the pane
-        maingridPane.setMinSize(400, 600);
+        Button btn1 = new Button("Save");
+        Button btn2 = new Button("Finish");
+        Button btn3 = new Button("Cancel");
+        btn3.setOnAction(e->nWin.close());
 
-        //Setting the padding
-        maingridPane.setPadding(new Insets(10, 10, 10, 10));
-
-        //Setting the vertical and horizontal gaps between the columns
-        maingridPane.setVgap(5);
-        maingridPane.setHgap(5);
-
-        //Setting the Grid alignment
-        maingridPane.setAlignment(Pos.CENTER);
-
-        //Arranging all the nodes in the grid
-        maingridPane.add(text1, 0, 0);
-        maingridPane.add(textField1, 1, 0);
-        maingridPane.add(text2, 0, 1);
-        maingridPane.add(textField2, 1, 1);
-        maingridPane.add(text3, 0, 2);
-        maingridPane.add(textField3, 1, 2);
-        maingridPane.add(text4, 0, 3);
-        maingridPane.add(textField4, 1, 3);
-        maingridPane.add(button1, 0, 4);
-        maingridPane.add(button2, 1, 4);
-
-        //Styling nodes
-        button1.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
-        button1.setOnAction(actionEvent -> test(textField1,textField2,textField3,textField4));
-        button2.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
-        button2.setOnAction(actionEvent -> createGalamsey());
-
-
-        text1.setStyle("-fx-font: normal bold 20px 'serif' ");
-        text2.setStyle("-fx-font: normal bold 20px 'serif' ");
-        text3.setStyle("-fx-font: normal bold 20px 'serif' ");
-        text4.setStyle("-fx-font: normal bold 20px 'serif' ");
-
-
-        maingridPane.setStyle("-fx-background-color: BEIGE;");
-
-        initScene = new Scene(maingridPane);
-        nWin.setTitle("Create new observatory");
-        nWin.setScene(initScene);
-
-        //Displaying the contents of the stage
-        nWin.showAndWait();
-    }
-    public static void createGalamsey(){
-        //Scene B
-        VBox mainpane = new VBox(5);
-        HBox row1 = new HBox(5);
-        HBox row2 = new HBox(15);
-        HBox row3 = new HBox(5);
-
-        Label l1 = new Label("Events");
-        Text tx1 = new Text("Start year");
-        Text tx2 = new Text("Cooordinates");
-        Text tx3 = new Text("Vegetation colllour");
-        Button b1 = new Button("Add new event");
-
-        row1.getChildren().add(l1);
-        row2.getChildren().add(b1);
-        row3.getChildren().addAll(tx1,tx2,tx3);
-
-        mainpane.getChildren().addAll(row1,row2,row3);
-        mainpane.setStyle("-fx-background-color: BEIGE;");
-        row1.setAlignment(Pos.CENTER);
-        row2.setAlignment(Pos.CENTER_RIGHT);
-
-        Scene newScene1 = new Scene(mainpane,500,600);
-
-
-        nWin.setScene(newScene1);
-        nWin.show();
-
-        //Scene A
-        VBox pane = new VBox(15);
-        HBox inpane1 = new HBox(5);
-        HBox inpane2 = new HBox(5);
-        HBox inpane3 = new HBox(5);
-        HBox inpane4 = new HBox(5);
-        HBox inpane5 = new HBox(5);
-
-        Text t1 = new Text("Latitude");
-        Text t2 = new Text("Longitude");
-        Text t3 = new Text("Start year");
-        Text t4 = new Text("Vegetation colour");
-
-        TextField textField1 = new TextField();
-        TextField textField2 = new TextField();
-        TextField textField3 = new TextField();
-        TextField textField4 = new TextField();
-
-
-        Button btn1 = new Button("Done");
-
-        Button btn2 = new Button("Cancel");
-
+        rightpane.setMinSize(400,600);
+        form.setMinSize(400, 600);
+        form.setPadding(new Insets(10, 10, 10, 10));
+        form.setVgap(5);
+        form.setHgap(5);
+        form.setAlignment(Pos.CENTER);
+        form.setStyle("-fx-background-color: BEIGE;");
 
         t1.setStyle("-fx-font: normal bold 20px 'serif' ");
         t2.setStyle("-fx-font: normal bold 20px 'serif' ");
         t3.setStyle("-fx-font: normal bold 20px 'serif' ");
         t4.setStyle("-fx-font: normal bold 20px 'serif' ");
+        t5.setStyle("-fx-font: normal bold 20px 'serif' ");
+        t6.setStyle("-fx-font: normal bold 20px 'serif' ");
+        t7.setStyle("-fx-font: normal bold 20px 'serif' ");
+        t8.setStyle("-fx-font: normal bold 20px 'serif' ");
+        t9.setStyle("-fx-font: normal bold 20px 'serif' ");
+        t10.setStyle("-fx-font: normal bold 20px 'serif' ");
+        t11.setStyle("-fx-font: normal bold 20px 'serif' ");
 
+        l1.setStyle("-fx-font: normal bold 20px 'serif' ");
+        l2.setStyle("-fx-font: normal bold 20px 'serif' ");
+        l3.setStyle("-fx-font: normal bold 20px 'serif' ");
 
-        pane.setStyle("-fx-background-color: BEIGE;");
+        form.setStyle("-fx-background-color: BEIGE;");
+        rightpane.setStyle("-fx-background-color: BEIGE;");
+
         btn1.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
         btn2.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
+        btn3.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
 
-        inpane1.setAlignment(Pos.CENTER);
-        inpane2.setAlignment(Pos.CENTER);
-        inpane3.setAlignment(Pos.CENTER);
-        inpane4.setAlignment(Pos.CENTER);
-        inpane5.setAlignment(Pos.CENTER);
+        form.add(l4,0,0);
+        form.add(t8,0, 1);
+        form.add(textField1, 1,1);
+        form.add(t9,0,2);
+        form.add(textField2,1 , 2);
+        form.add(t10,0,3);
+        form.add(textField3,1,3);
+        form.add(t11,0,4);
+        form.add(textField4,1,4);
+        form.add(l1,0,6);
+        form.add(t4,0,7);
+        form.add(textField5,1,7);
+        form.add(t5,0,8);
+        form.add(textField6,1,8);
+        form.add(t6,0,9);
+        form.add(textField7,1,9);
+        form.add(t7,0,10);
+        form.add(textField8,1,10);
+        form.add(btn1,1,12);
 
-        inpane1.getChildren().addAll(t1,textField1);
-        inpane2.getChildren().addAll(t2,textField2);
-        inpane3.getChildren().addAll(t3,textField3);
-        inpane4.getChildren().addAll(t4,textField4);
-        inpane5.getChildren().addAll(btn1,btn2);
-        pane.getChildren().addAll(inpane1,inpane2,inpane3,inpane4,inpane5);
-        pane.setAlignment(Pos.CENTER);
-        Scene newScene = new Scene(pane,400,600);
+        //Row for columns
+        HBox row1= new HBox(5);
+        row1.setAlignment(Pos.CENTER);
+        row1.getChildren().add(l2);
 
+        HBox row2= new HBox(5);
+        row2.getChildren().addAll(t1,t2,t3);
+
+        rightpane.getChildren().addAll(row1,row2);
+
+        //Row for buttons
+        HBox row3= new HBox(10);
+        row3.setAlignment(Pos.CENTER);
+        row3.getChildren().addAll(btn2,btn3);
+
+        mainPane.setBottom(row3);
+
+        btn2.setOnAction(e->nWin.close());
 
         btn1.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                HBox newRow = new HBox(5);
-                Text tt1 = new Text(textField1.getText());
-                Text tt2 = new Text(textField2.getText());
-                Text tt3 = new Text(textField3.getText());
-                Text tt4 = new Text(textField4.getText());
-                newRow.getChildren().addAll(tt1,tt2,tt3,tt4);
-                maingridPane.getChildren().add(newRow);
-                nWin.setScene(newScene);
+                Text s = new Text(textField7.getText());
+                Text cord = new Text("("+textField5.getText()+", "+textField6.getText()+")" );
+                Text col = new Text(textField8.getText());
+                HBox row = new HBox(90);
+                row.getChildren().addAll(s,cord,col);
+                rightpane.getChildren().addAll(row);
                 nWin.show();
             }
         });
 
 
+        initScene = new Scene(mainPane,1000,600);
+        nWin.setTitle("Create new observatory");
+        nWin.setScene(initScene);
+        nWin.show();
+    }
+
+    public static void validate(Button save){
 
     }
+
     public static void test(TextField name,TextField country,TextField year,TextField area){
         if(testArea(area.getCharacters().toString())==false){
             test(name,country,year,area);
